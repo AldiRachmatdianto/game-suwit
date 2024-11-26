b29103c6426a7cdf089248709acbf38501bc791b
Aldi Rachmatdianto <aldi@example.com>
2024-11-26 17:13:02 +0700

    update ajah

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-let main = true;
+let h1 = document.getElementById("h1");
+let nama = document.getElementById("nama");
+
+nama.innerHTML = prompt("Siapa Nama Anda?");
+let gantiNama = nama.textContent;
+gantiNama = gantiNama.toUpperCase();
+
+
+
+
+let main = confirm("Ingin Bermain Suwit?");
+
 while (main) {
 
-        let player = prompt("Pilih: \n1. Gunting \n2. Batu \n3. Kertas");
+        let player = prompt("Pilih: \n- Gunting \n- Batu \n- Kertas");
         let lawan = Math.random();
         if (lawan < 0.36) {
 
@@ -22,21 +33,28 @@ while (main) {
             hasil = "SERI COYYYYYYYY";
             
         }
-         else if (player == "Batu"){
+         else if (player == "Batu" || player == "batu"){
 
             hasil = (lawan == "Kertas") ? "NT NT" : "MENANGG";
 
-        } else if (player == "Kertas") {
+        } else if (player == "Kertas" || player == "kertas") {
 
             hasil = (lawan == "Gunting") ? "NT NT" : "MENANGG";
 
-        } else if (player == "Gunting"){
+        } else if (player == "Gunting" || player == "gunting"){
             hasil = (lawan == "Batu") ? "NT NT" : "MENANGG";
         } else {
             hasil = "YA GAK TAU MAKANYA YANG BENER MASUKINNYA KONTOL";
         }
         alert(`Anda Memilih: ${player} \nLawan Memilih: ${lawan} \nHasil: ${hasil}`);
         main = confirm("Coba lagi?");
+        
+        if (main === false) {
+
+            h1.innerHTML = `SEMOGA BERJUMPA LAGI ${gantiNama}!!!!!!`
+        }
     
 }
 alert("Terima kasih telah bermain");
+
+
